592622fb4a721d38bdf0179c759308bd582c9c60
Aaron Sinclair <aaron@selfienetworks.com>
2020-12-01 16:10:20 -0800

    Adding CMake build scripts & unit tests

---
+++ tests/test_nte.cc
@@ -0,0 +1,18 @@
+#include <UnitTest++/UnitTest++.h>
+#include <jansson.h>
+#include <string>
+#include "utils/nte.h"
+
+
+TEST(DecryptValidToken) {
+    const std::string key( "{\"alg\":\"A128CBC-HS256\",\"k\":\"Qr_XwDGctna3SlR88rEJYt6Zm100SASYeJWSJihDnsA\",\"kty\":\"oct\"}" );
+    const std::string token( "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..tO-hxpVQjViPNpuELFqrPw.wLwLl0PuMS6oMmAHPvlZqtdNY0xkbOnIi0OCz0iOvJoLbviO-WBgYbVRWkqumvS3camvfEfSTmTrkyMZWOi8xjJD0KSvULCO3XTtCJSml-E.t9-RpE4j1xLDV-a4oHjmtg" );
+
+    json_t * decrypted_token = nte_decrypt(
+            token.data(), token.size(),
+            key.data(), key.size()
+    );
+    CHECK( decrypted_token );
+
+    json_decref( decrypted_token );
+}
