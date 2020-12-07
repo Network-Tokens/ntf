dadb75a175fe63d4e71d3010b130bbdf779b4ac3
Aaron Sinclair <aaron@selfienetworks.com>
2020-12-06 23:20:42 -0800

    Got new NTF module working in BESS

--- extra-ntf.mk
+++ extra-ntf.mk
@@ -1,2 +1,2 @@
 CXXFLAGS += -I/opt/ntf/include
-LIBS += -Wl,-Bstatic -lcjose -ljansson -lcrypto -Wl,-Bdynamic
+LIBS += -Wl,-Bstatic -lcjose -ljansson -lcrypto -L/opt/ntf/api -Wl,--whole-archive -lapi -Wl,--no-whole-archive -Wl,-Bdynamic
