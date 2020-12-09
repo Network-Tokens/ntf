9012c7a02e2a18a38ea7b72604d4688c45853b80
Aaron Sinclair <aaron@selfienetworks.com>
2020-12-08 20:39:33 -0800

    Removed unneeded library

--- extra-ntf.mk
+++ extra-ntf.mk
@@ -1,2 +1,2 @@
 CXXFLAGS += -I/opt/ntf/include
-LIBS += -Wl,--whole-archive -L/opt/ntf/api -lapi -Wl,--no-whole-archive -Wl,-Bstatic -lcjose -ljansson -lcrypto -Wl,-Bdynamic
+LIBS += -Wl,-Bstatic -lcjose -ljansson -lcrypto -Wl,-Bdynamic
