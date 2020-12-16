480e204250c034b2f9622e4612fd67c9460ae458
Aaron Sinclair <aaron@selfienetworks.com>
2020-12-15 16:38:56 -0800

    Clean up some google stuff on test exit

--- tests/test_main.cc
+++ tests/test_main.cc
@@ -1,8 +1,14 @@
 #include <UnitTest++/UnitTest++.h>
 #include <glog/logging.h>
+#include <google/protobuf/stubs/common.h>
 
 
-int main(int, char *argv[]) {
-    google::InitGoogleLogging(argv[0]);
+int main( int, char *argv[] ) {
+    google::InitGoogleLogging( argv[0] );
+
     UnitTest::RunAllTests();
+
+    google::protobuf::ShutdownProtobufLibrary();
+    google::ShutdownGoogleLogging();
+    google::ShutDownCommandLineFlags();
 }
